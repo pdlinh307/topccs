40c92db09041f96876f1533c61709242e74dbed4
kingeric <linhpd@realtel.vn>
2018-06-24 17:11:10 +0700

    Bổ sung readme. Tạo schema table.

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# Intro
-
-# Version
-
-# Install & deploy
-
+# Installation
+## Database
+Type: MySQL <br>
+Database: **topccs** <br>
+Import Command:
+```bash
+mysql -u <user> -p topccs < topccs.sql
+```
+## Python
+### Install python
+Version **3.6 or later**
+```bash
+sudo yum install python3.6
+python3 --version
+```
+### Install pip
+Download [get-pip.py](https://bootstrap.pypa.io/get-pip.py)
+```bash
+sudo python3 get-pip.py
+``` 
+### Install required libs
+```bash
+cd /path/to/project
+sudo pip3 install -r requirements.txt
+```
+# Configuration
+## Supervisor
+## Gunicorn
+## Nginx
 # API specs
+|#      |URL    |Input  | Output    |
+|:-------------|:-------------|:-------------|:-----|
+|1    |   |   |   |
--- topccs.sql
+++ topccs.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.11, for Linux (x86_64)
 --
 -- Host: localhost    Database: topccs
 -- ------------------------------------------------------
--- Server version	5.7.22-0ubuntu0.16.04.1
+-- Server version	8.0.11
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8mb4 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,12 +16,101 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `topccs`
+-- Table structure for table `campaigns`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `topccs` /*!40100 DEFAULT CHARACTER SET utf8 */;
+DROP TABLE IF EXISTS `campaigns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `campaigns` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `campaign_id` int(11) NOT NULL,
+  `type_id` int(11) DEFAULT '0',
+  `code` varchar(16) DEFAULT NULL,
+  `time_start` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `time_end` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `time_last_call` datetime DEFAULT NULL,
+  `status_received` tinyint(4) NOT NULL DEFAULT '0',
+  `status_scheduled` tinyint(4) NOT NULL DEFAULT '0',
+  `status_completed` tinyint(4) NOT NULL DEFAULT '0',
+  `round` int(4) NOT NULL DEFAULT '0',
+  `number_contacts` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_campaign_id` (`campaign_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `topccs`;
+--
+-- Dumping data for table `campaigns`
+--
+
+LOCK TABLES `campaigns` WRITE;
+/*!40000 ALTER TABLE `campaigns` DISABLE KEYS */;
+/*!40000 ALTER TABLE `campaigns` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cdr`
+--
+
+DROP TABLE IF EXISTS `cdr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `cdr` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `campaign_id` int(11) NOT NULL,
+  `crm_contact_id` int(11) NOT NULL,
+  `disposition` varchar(45) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `answer_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `billsec` int(11) DEFAULT NULL,
+  `record_url` varchar(128) DEFAULT NULL,
+  `linkedid` varchar(64) DEFAULT NULL,
+  `uniqueid` varchar(64) DEFAULT NULL,
+  `agentcode` varchar(45) DEFAULT NULL,
+  `station_id` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cdr`
+--
+
+LOCK TABLES `cdr` WRITE;
+/*!40000 ALTER TABLE `cdr` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cdr` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `crm_contact_id` int(11) NOT NULL,
+  `phone_number` varchar(45) NOT NULL,
+  `linkedit` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_contact_id` (`crm_contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -32,4 +121,4 @@ USE `topccs`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-23 15:51:01
+-- Dump completed on 2018-06-24 16:37:19
