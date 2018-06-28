fa14b90345cb535bbaef2c86a5a1bd743854f537
King Eric <linhpd@realtel.vn>
2018-06-28 16:29:55 +0700

    Resolved issue #2

--- topccs.sql
+++ topccs.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+-- MySQL dump 10.16  Distrib 10.3.7-MariaDB, for Linux (x86_64)
 --
--- Host: labcen    Database: topccs
+-- Host: localhost    Database: topccs
 -- ------------------------------------------------------
--- Server version	8.0.11
+-- Server version	10.3.7-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,16 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `topccs`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `topccs`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `topccs` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `topccs`;
+
+--
 -- Table structure for table `campaigns`
 --
 
@@ -24,30 +34,22 @@ DROP TABLE IF EXISTS `campaigns`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `campaigns` (
   `campaign_id` int(11) NOT NULL,
-  `type_id` int(11) DEFAULT '0',
-  `time_start` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type_id` int(11) DEFAULT 0,
+  `time_start` datetime NOT NULL DEFAULT current_timestamp(),
   `time_end` datetime DEFAULT NULL,
   `time_last_call` datetime DEFAULT NULL,
-  `status_received` tinyint(1) NOT NULL DEFAULT '1',
-  `status_scheduled` tinyint(1) NOT NULL DEFAULT '0',
-  `status_completed` tinyint(1) NOT NULL DEFAULT '0',
-  `status_closed` tinyint(1) NOT NULL DEFAULT '0',
-  `round` int(4) NOT NULL DEFAULT '0',
-  `number_contacts` int(11) NOT NULL DEFAULT '0',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status_received` tinyint(1) NOT NULL DEFAULT 1,
+  `status_scheduled` tinyint(1) NOT NULL DEFAULT 0,
+  `status_completed` tinyint(1) NOT NULL DEFAULT 0,
+  `status_closed` tinyint(1) NOT NULL DEFAULT 0,
+  `round` int(4) NOT NULL DEFAULT 0,
+  `number_contacts` int(11) NOT NULL DEFAULT 0,
+  `number_contacts_success` int(11) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`campaign_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `campaigns`
---
-
-LOCK TABLES `campaigns` WRITE;
-/*!40000 ALTER TABLE `campaigns` DISABLE KEYS */;
-/*!40000 ALTER TABLE `campaigns` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `cdr`
@@ -73,37 +75,60 @@ CREATE TABLE `cdr` (
   `record_url` varchar(128) DEFAULT NULL,
   `uniqueid` varchar(64) DEFAULT NULL,
   `linkedid` varchar(64) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `cdr_campaign_id_contact_id_uindex` (`campaign_id`,`contact_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cdr`
---
-
-LOCK TABLES `cdr` WRITE;
-/*!40000 ALTER TABLE `cdr` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cdr` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-/*!50032 DROP TRIGGER IF EXISTS trigger_increase_contact */;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`linhpd`@`%`*/ /*!50003 TRIGGER `trigger_increase_contact` AFTER INSERT ON `cdr` FOR EACH ROW update campaigns set number_contacts = number_contacts + 1 where campaign_id = NEW.campaign_id */;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`topccs`@`%`*/ /*!50003 TRIGGER `trigger_increase_contact`
+  AFTER INSERT
+  ON `cdr`
+  FOR EACH ROW
+  update `campaigns`
+  set number_contacts = number_contacts + 1
+  where campaign_id = NEW.campaign_id */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`topccs`@`%`*/ /*!50003 TRIGGER `trigger_increase_contact_success`
+  AFTER UPDATE
+  ON `cdr`
+  FOR EACH ROW
+  BEGIN
+    IF NEW.disposition = 'ANSWERED' THEN
+      update `campaigns` set `number_contacts_success` = `number_contacts_success` + 1 where `campaign_id` = NEW.campaign_id;
+    end if;
+  END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping events for database 'topccs'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -114,4 +139,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-27 16:33:52
+-- Dump completed on 2018-06-28 17:00:33
